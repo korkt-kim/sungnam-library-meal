2cf35e737262780be6851cb0ec53a5dc2b7fc5ad
korkt-kim <gnscott6@naver.com>
2026-02-22 16:51:35 +0900

    chore: initial project setup
    - Configure ESLint and Prettier for code quality
    
    - Integrate Ant Design (ConfigProvider in root)
    
    - Enable file-system routing (@react-router/fs-routes)
    
    - Add .vscode to gitignore

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSameLine: true,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 80,
+  trailingComma: 'all',
+  useTabs: false,
+  semi: false,
+}
